2944a77f77aef683f69b4051ca71ab6bca787419
masdefry <muhammaddefryan@gmail.com>
2025-07-17 16:54:45 +0700

    intro to next.js

---
+++ intro-nextjs/src/app/profile/page.tsx
@@ -0,0 +1,14 @@
+export default function Profile() {
+  return (
+    <>
+      <h1
+        style={{ backgroundColor: 'green', color: 'white', fontSize: '100px' }}
+      >
+        Hi ni Kak Defryan
+      </h1>
+      <h2 className='jcwd'>
+        Job Connector Web Development
+      </h2>
+    </>
+  );
+}
